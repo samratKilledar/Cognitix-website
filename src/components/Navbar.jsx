9e90bdf0f81cf0bc55f277066be03b42c5dfbd57
samratkilledar <samratkilledar@gmail.com>
2026-03-01 16:09:59 +0530

    Added all code OF UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,24 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./navbar.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <h2>My Website</h2>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/contact">Contact</Link>
+      <h2>Cognitix PVT. LTD. Kolhapur</h2>
+
+      <div className={`nav-links ${menuOpen ? "active" : ""}`}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+        <Link to="/about" onClick={() => setMenuOpen(false)}>About</Link>
+        <Link to="/services" onClick={() => setMenuOpen(false)}>Services</Link>
+        <Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
+      </div>
+
+      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
       </div>
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,53 @@ import Services from "../components/Services";
 import States from "../components/States";
 import CTA from "../components/CTA";
 
+import { useEffect, useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as SiIcons from "react-icons/si";
+
 export default function Home() {
+
+  const [icons, setIcons] = useState([]);
+
+  useEffect(() => {
+    const allIcons = { ...FaIcons, ...SiIcons };
+    const iconKeys = Object.keys(allIcons);
+
+    const generatedIcons = Array.from({ length: 100 }).map(() => {
+      const randomIcon =
+        allIcons[iconKeys[Math.floor(Math.random() * iconKeys.length)]];
+      return {
+        Icon: randomIcon,
+        left: Math.random() * 100,
+        delay: Math.random() * 5,
+        size: Math.random() * 20 + 20,
+        duration: Math.random() * 10 + 10,
+      };
+    });
+
+    setIcons(generatedIcons);
+  }, []);
+
   return (
     <>
+      <div className="icon-waterfall">
+        {icons.map((item, index) => {
+          const IconComponent = item.Icon;
+          return (
+            <IconComponent
+              key={index}
+              className="falling-icon"
+              style={{
+                left: `${item.left}%`,
+                fontSize: `${item.size}px`,
+                animationDelay: `${item.delay}s`,
+                animationDuration: `${item.duration}s`,
+              }}
+            />
+          );
+        })}
+      </div>
+
       <Hero />
       <Services />
       <States />
