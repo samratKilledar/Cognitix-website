1af71cd547cb6a284a7a36104ab4367c4f9ee340
patilsakshi438-creator <patilsakshi438@gmail.com>
2026-03-01 12:58:35 +0530

    create new home page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+function HomePage() {
+  return (
+    <div className="home">
+      <header className="home-header">
+        <h1>Welcome to My Website</h1>
+        <p>This is a simple home page built with React JS.</p>
+        <button className="btn">Get Started</button>
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
