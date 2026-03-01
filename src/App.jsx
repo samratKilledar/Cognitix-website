b14eaf4f20e33ab3f195f33dbab2f664312b0f37
patilsakshi438-creator <patilsakshi438@gmail.com>
2026-03-01 16:40:24 +0530

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 // import Footer from "./components/Footer";
-//import Home from "./pages/Home";
-import HomePage from "./pages/HomePage";
+import Home from "./pages/Home";
+//import HomePage from "./pages/HomePage";
 // import About from "./pages/About";
 // import Services from "./pages/Services";
 // import Contact from "./pages/Contact";
@@ -13,8 +13,8 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
-         {/* <Route path="/" element={<Home />} />  */}
-         <Route path="/" element={<HomePage />} /> 
+         <Route path="/" element={<Home />} />  
+         {/* <Route path="/" element={<HomePage />} />  */}
         {/* <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} /> */}
