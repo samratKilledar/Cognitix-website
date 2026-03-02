05c8bf4111ab7a15c1f5c0d43c6f285306ec0ccf
patilkomal7030-source <patilkomal7030@gmail.com>
2026-03-02 13:42:06 +0530

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./pages/Home";
 // import About from "./pages/About";
  import Services from "./pages/Services";
 // import Contact from "./pages/Contact";
-//import Aboutuspage from "./pages/Aboutuspage";
+import Aboutuspage from "./pages/Aboutuspage";
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
         {/* <Route path="/about" element={<About />} /> */}
         <Route path="/services" element={<Services />} />
        {/* <Route path="/contact" element={<Contact />} /> */}
-       {/* <Route path="/Aboutuspage" element={<Aboutuspage />} /> */}
+        <Route path="/Aboutuspage" element={<Aboutuspage />} /> 
         
       </Routes>
       {/* <Footer /> */}
